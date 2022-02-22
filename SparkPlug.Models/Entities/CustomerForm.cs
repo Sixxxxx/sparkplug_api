590d8a533080537e2637dca5fd29191a28819736
six <jcuudeh@gmail.com>
2022-02-22 11:16:01 +0100

    setup models enums and settings

---
+++ SparkPlug.Models/Entities/CustomerForm.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SparkPlug.Models.Entities
+{
+    public class CustomerForm
+    {
+        public CustomerForm()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = default!;
+        public string CustomerName { get; set; } = default!;
+        public string CustomerEmail { get; set; } = default!;
+        public string CustomerMessage { get; set; } = default!;
+        public DateTime DateCreated { get; set; } = default!;
+    }
+}
---
+++ SparkPlug.Services/Infrastructures/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿
+using SparkPlug.Services.Interfaces;
+
+namespace SparkPlug.Services.Infrastructures
+{
+    public class DatabaseSettings 
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string FormsCollectionName { get; set; }
+    }
+}
